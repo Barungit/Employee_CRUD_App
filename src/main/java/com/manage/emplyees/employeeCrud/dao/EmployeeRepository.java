77034c70e5ed4fba18b376cc8c3eed488b8f68f6
Barungit <barunkr0801@gmail.com>
2023-09-29 19:33:13 +0530

    changing default url and page size

--- src/main/java/com/manage/emplyees/employeeCrud/dao/EmployeeRepository.java
+++ src/main/java/com/manage/emplyees/employeeCrud/dao/EmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.manage.emplyees.employeeCrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.manage.emplyees.employeeCrud.entity.Employee;
-
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
