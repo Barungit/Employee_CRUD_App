85ba7d1041725245fc3a4ca5368c69daecd60577
Barungit <barunkr0801@gmail.com>
2023-09-30 20:13:11 +0530

    Adding Spring Security

--- src/main/java/com/manage/emplyees/employeeCrud/dao/EmployeeRepository.java
+++ src/main/java/com/manage/emplyees/employeeCrud/dao/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.manage.emplyees.employeeCrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.manage.emplyees.employeeCrud.entity.Employee;
 //@RepositoryRestResource(path="members")
